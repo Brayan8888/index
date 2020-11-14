faf6c1d1a2748e9f460049d01b5ad9b9d9201dd1
Brayan8888 <74459336+Brayan8888@users.noreply.github.com>
2020-11-14 16:25:41 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,33 @@
-
+let hello="hello";
+let com="com";
+let ua="ua";
+let bool1=true;
+let bool2=false;
+let a=1;
+let b=10;
+let c=15;
+console.log(hello);
+console.log(com);
+console.log(ua);
+console.log(1);
+console.log(10);
+console.log(15);
+console.log(5*2<6);
+let mom={
+    name:'jola',
+    age:47,
+}
+let arrName=["Yana","jana"]
+let arrCountry=["Ukraine","Rusia"]
+console.log(arrName);
+console.log(arrCountry);
+console.log(arrName[1]);
+document.body.style.background='grey';
+let student={
+    name="Tom",
+    age=18,
+    skills:["html","css","js"],
+}
+function nameFunc{
+    
+}
